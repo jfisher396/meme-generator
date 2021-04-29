4a7c8870fb6fd8173228617501af3cf3876f8636
James Fisher <jfisher396@hotmail.com>
2021-04-29 16:04:14 -0700

    created componentDidMount for api fetch

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react'
 
 class MemeGenerator extends Component {
+  constructor() {
+    super();
+    this.state = {
+      topText: "",
+      bottomText: "",
+      randomImg: "http://i.imgflip.com/1bij.jpg",
+      allMemeImgs: [],
+    };
+  }
 
-    constructor() {
-        super()
-        this.state = {}
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Meme Generator</h1>
-            </div>
-        )
-    }
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((response) => response.json())
+      .then((response) => {
+        const { memes } = response.data;
+        this.setState({allMemeImgs: memes})
+      });
+    
+  }
+    
+  render() {
+      
+    return (
+      <div>
+        <h1>Meme Generator</h1>
+      </div>
+    );
+  }
 }
 
 export default MemeGenerator

cfca25a3bca3d3ccaaabb9d1e422798e5f16367c
James Fisher <jfisher396@hotmail.com>
2021-04-29 16:45:16 -0700

    inputChangeHandler bound to this keyword

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 
 class MemeGenerator extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       topText: "",
       bottomText: "",
       randomImg: "http://i.imgflip.com/1bij.jpg",
       allMemeImgs: [],
-    };
+    }
+    this.inputChangeHandler = this.inputChangeHandler.bind(this)
   }
 
   componentDidMount() {
@@ -16,19 +17,38 @@ class MemeGenerator extends Component {
       .then((response) => response.json())
       .then((response) => {
         const { memes } = response.data;
-        this.setState({allMemeImgs: memes})
+        this.setState({ allMemeImgs: memes })
       });
-    
   }
-    
+
+  inputChangeHandler(event) {
+      const { name, value } = event.target
+      this.setState({[name]: value})
+  }
+
   render() {
-      
     return (
       <div>
-        <h1>Meme Generator</h1>
+        <form className="meme-form">
+          <input
+            type="text"
+            name="topText"
+            value={this.state.topText}
+            placeholder="Top Text"
+            onChange={this.inputChangeHandler}
+          />
+          <input
+            type="text"
+            name="bottomText"
+            value={this.state.bottomText}
+            placeholder="Bottom Text"
+            onChange={this.inputChangeHandler}
+          />
+          <button>Gen</button>
+        </form>
       </div>
     );
   }
 }
 
-export default MemeGenerator
+export default MemeGenerator;
